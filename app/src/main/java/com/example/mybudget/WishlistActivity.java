09a5a3206d31743d64a0d62c16d5e1e0268ee474
Beadsley <danbeadleson@gmail.com>
2019-04-09 14:16:59 +0200

    bottom navigation bar added -allows navigation between the main activity, account actvity and  wishlist activity

--- app/src/main/java/com/example/mybudget/WishlistActivity.java
+++ app/src/main/java/com/example/mybudget/WishlistActivity.java
@@ -2,19 +2,18 @@ package com.example.mybudget;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.design.widget.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class WishlistActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_wishlist);
         /*
          * Method creates a pathway to the other
          * activities via a navigation bar
@@ -22,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         Menu menu = navigation.getMenu();
-        MenuItem menuItem =menu.getItem(0);
+        MenuItem menuItem =menu.getItem(1);
         menuItem.setChecked(true);
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -31,25 +30,21 @@ public class MainActivity extends AppCompatActivity {
 
                 switch (id) {
                     case R.id.nav_home:
+                        Intent intent1= new Intent(WishlistActivity.this, MainActivity.class);
+                        startActivity(intent1);
                         break;
 
                     case R.id.nav_wishlist:
-                         Intent intent1 =new Intent(MainActivity.this, WishlistActivity.class);
-                         startActivity(intent1);
-                         break;
+                        break;
 
                     case R.id.nav_account:
-                         Intent intent2 = new Intent(MainActivity.this, AccountActivity.class);
-                         startActivity(intent2);
-                         break;
+                        Intent intent2 = new Intent(WishlistActivity.this, AccountActivity.class);
+                        startActivity(intent2);
+                        break;
 
                 }
                 return false;
             }
         });
-
-
-
-
     }
 }
