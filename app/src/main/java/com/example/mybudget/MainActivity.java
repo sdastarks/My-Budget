5beeb2e997732ad9cd3b52c6dd408f772c5ea64e
Beadsley <danbeadleson@gmail.com>
2019-04-12 11:07:49 +0200

    side navigation bar added

--- app/src/main/java/com/example/mybudget/MainActivity.java
+++ app/src/main/java/com/example/mybudget/MainActivity.java
@@ -10,8 +10,12 @@ package com.example.mybudget;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.design.widget.BottomNavigationView;
@@ -22,6 +26,9 @@ import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+
     private static  final String TAG= "MainActivity";
     protected Boolean inflow;
     myDbHelper db = new myDbHelper(this, "myDb.db", null, 1);
@@ -29,6 +36,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        drawer =findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
 
         /*
          * Method creates a pathway to the other
@@ -70,6 +90,16 @@ public class MainActivity extends AppCompatActivity {
         setProgressBar();
 
     }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
     /*
      * Method sets the status of the progress bar
      */
