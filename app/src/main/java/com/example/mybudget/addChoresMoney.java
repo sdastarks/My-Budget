b9331f33fedae7428743fe570f5fcd8b8825fd31
AlaaAlSakka <eng.alaa.alsaqqa@gmail.com>
2019-04-15 11:07:19 +0200

    done whith chores activity

--- app/src/main/java/com/example/mybudget/addChoresMoney.java
+++ app/src/main/java/com/example/mybudget/addChoresMoney.java
@@ -13,7 +13,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-
 /**
  * Fragment allows the user to enter
  * an income, that will affect the balance
@@ -21,11 +20,12 @@ import android.widget.Toast;
  * @author Daniel Beadleson
  *
  */
-public class InflowOutflowFragment extends Fragment {
 
-    private static  final String TAG= "InflowOutflowFragment";
-    EditText mDescription;
-    EditText mAmount;
+public class addChoresMoney extends Fragment {
+
+    private static  final String TAG= "addChoresMoney";
+    EditText choresDescription;
+    EditText choresAmount;
     Boolean inflow;
     /*
      * Method creates the initial state of the
@@ -35,21 +35,21 @@ public class InflowOutflowFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view =inflater.inflate(R.layout.fragment_inflow_outflow, container, false);
+        View view =inflater.inflate(R.layout.fragment_add_chores_money, container, false);
 
         // the boolean expression inflow will show if the input by the user
         //  is an income or spending
-        inflow =((MainActivity) getActivity()).inflow;
+        inflow =((ChoresActivity) getActivity()).inflow;
 
-        ImageView image =view.findViewById(R.id.image_inflow_outflow);
+        ImageView image = view.findViewById(R.id.choresImage_inflow_outflow);
         if(inflow){
             image.setImageDrawable(getResources().getDrawable(R.drawable.image_inflow));
         }
         else {
             image.setImageDrawable(getResources().getDrawable(R.drawable.image_outflow));
         }
-        mDescription= (EditText) view.findViewById(R.id.choresDescription);
-        mAmount = view.findViewById(R.id.amount);
+        choresDescription = (EditText) view.findViewById(R.id.choresDescription);
+        choresAmount = view.findViewById(R.id.choresAmount);
 
         return view;
 
@@ -59,31 +59,34 @@ public class InflowOutflowFragment extends Fragment {
         Log.v(TAG, "onViewCreated inititialsed");
         Log.v(TAG, "inflow"+inflow);
 
-        FloatingActionButton saveButton= view.findViewById(R.id.floatingActionButton_saveIncome);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            /*
+        FloatingActionButton saveButton= view.findViewById(R.id.choresFloatingActionButton_saveIncome);
+       saveButton.setOnClickListener(new View.OnClickListener()
+       {
+           @Override
+           public void onClick(View v)
+           {
+           /*
              * Method gets the description and amount of the income
              */
-            @Override
-            public void onClick(View v) {
-
-                String description = mDescription.getText().toString();
+                String description = choresDescription.getText().toString();
                 Log.v(TAG,"description: " +description);
-                String sAmount= mAmount.getText().toString();
+                String sAmount= choresAmount.getText().toString();
 
-                if (description.isEmpty() | sAmount.isEmpty()){
+                if (description.isEmpty() | sAmount.isEmpty())
+                {
                     Toast.makeText(getActivity(),"Both fields must be filled",Toast.LENGTH_SHORT).show();
                 }
-                else{
-                    int amount= Integer.parseInt(sAmount);
+                else
+                    {
+                    double amount= Double.parseDouble(sAmount);
                     Log.v(TAG, "amount: "+ amount);
                     Intent intent = new Intent(getActivity(), MainActivity.class);
                     startActivity(intent);
-                }
+                    }
             }
         });
 
-        FloatingActionButton cancelButton = view.findViewById(R.id.floatingActionButton_cancelIncome);
+        FloatingActionButton cancelButton = view.findViewById(R.id.choresFloatingActionButton_cancelIncome);
         cancelButton.setOnClickListener(new View.OnClickListener() {
             /*
              * Method sends the user back to the main menu
@@ -92,7 +95,7 @@ public class InflowOutflowFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Log.v(TAG, "cancel button initialised");
-                Intent intent = new Intent(getActivity(), MainActivity.class);
+                Intent intent = new Intent(getActivity(), ChoresActivity.class);
                 startActivity(intent);
             }
         });
