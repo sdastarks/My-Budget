5323111ae66df89f0d27d3d37c4cf5b3740c8504
Beadsley <danbeadleson@gmail.com>
2019-04-16 13:54:25 +0200

    money can be added to a wish

--- app/src/main/java/com/example/mybudget/ChangeWishInflowOutflowFragment.java
+++ app/src/main/java/com/example/mybudget/ChangeWishInflowOutflowFragment.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.mybudget.Models.Entry;
+import com.example.mybudget.Models.WishList;
 
 import java.time.LocalDate;
 
@@ -21,8 +22,8 @@ import java.time.LocalDate;
 /**
  * Fragment allows the user to transfer
  * money from balance to wish savings
- * @author Anastasija Gurejeva
  *
+ * @author Anastasija Gurejeva
  */
 public class ChangeWishInflowOutflowFragment extends Fragment {
 
@@ -42,8 +43,8 @@ public class ChangeWishInflowOutflowFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_change_wish_inflow_outflow, container, false);
 
-       balance= ((WishlistActivity) getActivity()).db.balance();
-       Log.v(TAG, "balance: "+balance);
+        balance = ((WishlistActivity) getActivity()).db.balance();
+        Log.v(TAG, "balance: " + balance);
 
         // the boolean expression inflow will show if the input by the user
         // is an transfer to savings or removing from savings to balance
@@ -79,34 +80,60 @@ public class ChangeWishInflowOutflowFragment extends Fragment {
                 String sAmount = mAmount.getText().toString();
 
 
-                if (sAmount.isEmpty()){
-                    Toast.makeText(getActivity(),"You have transfered 0 sek",Toast.LENGTH_SHORT).show();
+                if (sAmount.isEmpty()) {
+                    Toast.makeText(getActivity(), "You have transfered 0 sek", Toast.LENGTH_SHORT).show();
                 } else if (Integer.parseInt(sAmount) > balance) {
                     Toast.makeText(getActivity(), "You don't have enough money on your account", Toast.LENGTH_SHORT).show();
                 } else {
                     //Adding an entry to log
                     int amount = Integer.parseInt(sAmount);
                     Log.v(TAG, "TransactionAmount: " + amount);
-                    String wish = "";
+                    String wishName = "";
+
                     Entry entry = new Entry();
                     entry.setDate(LocalDate.now());
                     entry.setAmount(Integer.parseInt(sAmount));
-
+                    int dbid = ((WishlistActivity) getActivity()).id;
+                    WishList wish2Update = ((WishlistActivity) getActivity()).db.returnWish(dbid);
+                    
                     if (inflow) {
                         entry.setTypeOfEntry(2);
-                        wish = ((WishlistActivity) getActivity()).mWishNames.get(((WishlistActivity) getActivity()).index)
+                        wishName = ((WishlistActivity) getActivity()).mWishNames.get(((WishlistActivity) getActivity()).index)
                                 + " wishlist transfer";
-                    }
-                    else if (!inflow) {
+                        if (amount > (wish2Update.getCost() - wish2Update.getSaved())) {
+                            Toast.makeText(getActivity(), "Your goal doesnt need that much money, try "+
+                                    (wish2Update.getCost() - wish2Update.getSaved())+" SEK", Toast.LENGTH_LONG).show();
+                        }
+                        else if((wish2Update.getCost() - wish2Update.getSaved())==0){
+                            //TODO show some avatar when reach the goal
+                            Toast.makeText(getActivity(), "You have reached your goal", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            ((WishlistActivity) getActivity()).db.updateWish(dbid, wish2Update.getTitle()
+                            , wish2Update.getCost(), amount + wish2Update.getSaved(),
+                            wish2Update.getImage());
+
+                            entry.setDesc(wishName);
+                            ((WishlistActivity) getActivity()).db.addEntry(entry);
+                        }
+
+                    } else if (!inflow) {
                         entry.setTypeOfEntry(1);
-                        wish = ((WishlistActivity) getActivity()).mWishNames.get(((WishlistActivity) getActivity()).index)
+                        wishName = ((WishlistActivity) getActivity()).mWishNames.get(((WishlistActivity) getActivity()).index)
                                 + " wishlist return to balance";
+                        if (amount < wish2Update.getSaved()) {
+                            ((WishlistActivity) getActivity()).db.updateWish(dbid, wish2Update.getTitle()
+                             , wish2Update.getCost(), wish2Update.getSaved() - amount,
+                             wish2Update.getImage());
+
+                            entry.setDesc(wishName);
+                            ((WishlistActivity) getActivity()).db.addEntry(entry);
+                        } else {
+                            Toast.makeText(getActivity(), "Numbers to big!", Toast.LENGTH_SHORT).show();
+                        }
+
                     }
-                    entry.setDesc(wish);
 
-                    Log.v(TAG, "wish: "+wish);
-
-                    ((WishlistActivity) getActivity()).db.addEntry(entry);
                 }
 
                 getFragmentManager()
