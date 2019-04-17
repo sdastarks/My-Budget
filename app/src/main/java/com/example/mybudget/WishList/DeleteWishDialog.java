29eed4bd9d49226e876815f9c7c31ba8e4a90a8a
Daniel Beadleson <danbeadleson@gmail.com>
2019-04-17 19:05:55 +0200

    Edit wish layout (#69)
    Edit wish layout improved

--- app/src/main/java/com/example/mybudget/WishList/DeleteWishDialog.java
+++ app/src/main/java/com/example/mybudget/WishList/DeleteWishDialog.java
@@ -4,8 +4,10 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.util.Log;
 
 import com.example.mybudget.R;
 import com.example.mybudget.WishList.WishFragment;
@@ -22,6 +24,8 @@ public class DeleteWishDialog extends AppCompatDialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final int index =getArguments().getInt("indexEdit");
+        Log.v("DeleteWishDialog", "index: "+getArguments().getInt("indexEdit"));
         AlertDialog.Builder builder= new AlertDialog.Builder(getActivity());
         builder.setTitle("Information")
                 .setMessage("Are you sure you would like to delete this wish?")
@@ -37,10 +41,14 @@ public class DeleteWishDialog extends AppCompatDialogFragment {
                 .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        EditWishFragment editWishFragmentCall = new EditWishFragment();
+                        Bundle args = new Bundle();
+                        args.putInt("indexEdit", index);
                         getFragmentManager()
                                 .beginTransaction()
-                                .replace(R.id.frame_wish_fragment, new WishFragment())
+                                .replace(R.id.frame_wish_fragment, editWishFragmentCall)
                                 .commit();
+                        editWishFragmentCall.setArguments(args);
                     }
                 });
         return builder.create();
