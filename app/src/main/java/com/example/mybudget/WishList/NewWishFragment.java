1b5bbfea16eaef641342b8ac9ba07fc378c464ec
AnastasijaGurejeva <47104113+AnastasijaGurejeva@users.noreply.github.com>
2019-04-22 14:40:42 +0200

    Update NewWishFragment.java

--- app/src/main/java/com/example/mybudget/WishList/NewWishFragment.java
+++ app/src/main/java/com/example/mybudget/WishList/NewWishFragment.java
@@ -59,60 +59,6 @@ public class NewWishFragment extends Fragment {
         cancelNewWish = view.findViewById(R.id.btn_cancel_new_wish);
         activateOnCancelNewWish();
         activateOnSaveNewWish();
-        return view;
-    }
-
-    private void activateOnCancelNewWish() {
-        cancelNewWish.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), WishlistActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    /*
-     * Method attempts to save a wish
-     */
-
-
-    private void activateOnSaveNewWish() {
-        saveNewWish.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    int cost =Integer.parseInt(mNewWishCost.getText().toString());
-                    String wishTitle=mNewWishTitle.getText().toString();
-                    if (cost > 10000000){
-                        mNewWishCost.setError("Wish must be less than 10M SEK");
-                    }
-                    else if (wishTitle.isEmpty()){
-                        mNewWishTitle.setError("Field cannot be empty");
-                    }
-
-                    else if (wishTitle.length()> 25){
-                        mNewWishTitle.setError("Choose a smaller wish name");
-                    }
-                    else {
-                        WishList wish = new WishList();
-                        wish.setTitle(wishTitle);
-                        wish.setCost(cost);
-                        wish.setSaved(0);
-                        //wish.setImage(wishPicture);
-                        mNewWishCost.setError(null);
-                        ((WishlistActivity) getActivity()).db.addWish(wish);
-                        Intent intent = new Intent (getActivity(), WishlistActivity.class);
-                        startActivity(intent);
-                    }
-                }
-                catch (Exception e){
-                    mNewWishCost.setError("Try Again");
-                }
-            }
-        });
-
-
 
         theBikePic = view.findViewById(R.id.bike);
         theBikePic.setOnClickListener(new View.OnClickListener() {
@@ -204,10 +150,58 @@ public class NewWishFragment extends Fragment {
         });
 
 
-
-
-
         return view;
+    }
+    
+   private void activateOnCancelNewWish() {
+        cancelNewWish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), WishlistActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    /*
+     * Method attempts to save a wish
+     */
+
+
+    private void activateOnSaveNewWish() {
+        saveNewWish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    int cost =Integer.parseInt(mNewWishCost.getText().toString());
+                    String wishTitle=mNewWishTitle.getText().toString();
+                    if (cost > 10000000){
+                        mNewWishCost.setError("Wish must be less than 10M SEK");
+                    }
+                    else if (wishTitle.isEmpty()){
+                        mNewWishTitle.setError("Field cannot be empty");
+                    }
+
+                    else if (wishTitle.length()> 25){
+                        mNewWishTitle.setError("Choose a smaller wish name");
+                    }
+                    else {
+                        WishList wish = new WishList();
+                        wish.setTitle(wishTitle);
+                        wish.setCost(cost);
+                        wish.setSaved(0);
+                        //wish.setImage(wishPicture);
+                        mNewWishCost.setError(null);
+                        ((WishlistActivity) getActivity()).db.addWish(wish);
+                        Intent intent = new Intent (getActivity(), WishlistActivity.class);
+                        startActivity(intent);
+                    }
+                }
+                catch (Exception e){
+                    mNewWishCost.setError("Try Again");
+                }
+            }
+        });
     }
 
 }
