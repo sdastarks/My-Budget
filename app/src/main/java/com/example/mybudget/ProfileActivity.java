6f171692cb8c64c91dc3bee75ad9a7977deebb84
Benish Adnan <benish.khushnood@gmail.com>
2019-04-25 14:38:30 +0200

    Delete Bottom activity from profile activity

--- app/src/main/java/com/example/mybudget/ProfileActivity.java
+++ app/src/main/java/com/example/mybudget/ProfileActivity.java
@@ -3,20 +3,13 @@ package com.example.mybudget;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.mybudget.Account.AccountActivity;
-import com.example.mybudget.Chores.ChoresActivity;
 import com.example.mybudget.Home.MainActivity;
 import com.example.mybudget.Models.User;
-import com.example.mybudget.WishList.WishlistActivity;
 
 import static com.example.mybudget.Profile.RegisterActivity.USER_ID;
 import static com.example.mybudget.Profile.RegisterActivity.USER_PREFS_NAME;
@@ -80,40 +73,6 @@ public class ProfileActivity extends SettingsActivity {
             spendingsOfUser();
 
             } else Toast.makeText(this, "User is null", Toast.LENGTH_SHORT).show();
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation_profile);
-        Menu menu = navigation.getMenu();
-        MenuItem menuItem =menu.getItem(1);
-        menuItem.setChecked(true);
-        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-
-                switch (id) {
-                    case R.id.nav_home:
-                        Intent intent0= new Intent(ProfileActivity.this, MainActivity.class);
-                        startActivity(intent0);
-                        break;
-
-                    case R.id.nav_wishlist:
-                        Intent intent1 = new Intent(ProfileActivity.this, WishlistActivity.class);
-                        startActivity(intent1);
-                        break;
-
-                    case R.id.nav_account:
-                        Intent intent2 = new Intent(ProfileActivity.this, AccountActivity.class);
-                        startActivity(intent2);
-                        break;
-                    case R.id.nav_chores:
-                        Intent intent3 = new Intent(ProfileActivity.this, ChoresActivity.class);
-                        startActivity(intent3);
-                        break;
-
-                }
-                return false;
-            }
-        });
 
 
         /*imageviewCamera = (ImageView)findViewById(R.id.imageviewCamera);
