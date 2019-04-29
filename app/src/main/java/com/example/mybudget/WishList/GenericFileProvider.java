3caaf1966d9f27e20c07ae95f691d0682082f354
Benish Adnan <benish.khushnood@gmail.com>
2019-04-29 23:35:01 +0200

    camera partially working

---
+++ app/src/main/java/com/example/mybudget/WishList/GenericFileProvider.java
@@ -0,0 +1,10 @@
+package com.example.mybudget.WishList;
+
+import android.support.v4.content.FileProvider;
+
+public class GenericFileProvider extends FileProvider {
+   /* @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return(new LegacyCompatCursorWrapper(super.query(uri, projection, selection, selectionArgs, sortOrder)));
+    }*/
+}
